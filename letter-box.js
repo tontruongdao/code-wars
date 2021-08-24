6a7c2ef162a137301c8c5e5c103e46e87ef0133a
truongsinistar <truong@sinistar.ca>
2021-08-24 10:08:19 -0400

    letter paint completed

--- letter-box.js
+++ letter-box.js
@@ -1,10 +1,36 @@
-const paintLetterBoxes = (startNumber, endNumber) => {
+// Code Wars' Solution
+function paintLetterboxes(start, end) {
+  const frequencies = new Array(10).fill(0);
+  for(let number=start; number<=end; number++) {
+    number.toString().split('').forEach(x => frequencies[x]++);
+  }
+  return frequencies;
+}
 
-  console.log("hello world")
-  return ("hello world")
+
+// My Solution
+const paintLetterBoxes = (startNumber, endNumber) => {
+  const numMapArr =  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+
+  let start = startNumber;
+  let end = endNumber;
+  const rangeArr = []
+  while(start <= end) {
+    rangeArr.push(start);
+    start++;
+  }
+  console.log(rangeArr)
+
+  for (const num of rangeArr){
+    const numArr = String(num).split("").map((int) => Number(int))
+    numArr.forEach((int) => numMapArr[int]++)
+  }
+
+  console.log(numMapArr)
+  return numMapArr
 } 
 
-// paintLetterBoxes();
+paintLetterBoxes(8, 15);
 
 
 // Convert a number to an array.
@@ -28,23 +54,32 @@ while(start <= end) {
 
 
 // Making a number Map(object)
-const numberMap = {};
-const dummyNumber = [19, 200, 3018, 14];
+const numberMapObj = {};
+const dummyNumberObj = [19, 200, 3018, 14];
 // console.log(dummyNumber)
 
 // Iterating through each digit to record in number map.
-for (const num of dummyNumber){
+for (const num of dummyNumberObj){
   const numArr = String(num).split("").map((int) => Number(int))
 
   numArr.forEach((int) => {
-    if(numberMap[int]) {
-      numberMap[int] ++
+    if(numberMapObj[int]) {
+      numberMapObj[int] ++
     } else {
-      numberMap[int] = 1
+      numberMapObj[int] = 1
     }
   })
 }
-// console.log(numberMap)
+// console.log(numberMapObj)
 
 
 // Making a number Map(array)
+const numberMapArr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+const dummyNumberArr = [19, 200, 3018, 14];
+
+// Iterating through each digit to record in number map.
+for (const num of dummyNumberArr){
+  const numArr = String(num).split("").map((int) => Number(int))
+  numArr.forEach((int) => numberMapArr[int] ++)
+}
+// console.log(numberMapArr)
