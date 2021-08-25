8cdd794d7eeadb3d119e13172f6a84e58b589ba8
truongsinistar <truong@sinistar.ca>
2021-08-25 14:28:55 -0400

    completed mumbling

--- mumbling.js
+++ mumbling.js
@@ -1,8 +1,22 @@
-// My Solution
-const accum = (str) => {
-  console.log(str)
+// Code Wars Solution
+function accum(s) {
+  console.log(s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-'))
+  return s.split('').map((c, i) => (c.toUpperCase() + c.toLowerCase().repeat(i))).join('-');
 }
+accum("codewars")
 
+// My Solution
+const accumulate = (str) => {
+  const noWhiteSpaceString = str.replace(/\s/g, "");
+  const charArray = noWhiteSpaceString.split("")
+  const ajustedCaseArray = charArray.map((char, index) => (
+    char.toUpperCase() + char.toLowerCase().repeat(index)
+  ))
+  // console.log(ajustedCaseArray.join("-"))
+
+  return ajustedCaseArray.join("-")
+}
+// accumulate("hatter")
 
 // Remove white space from string
 const testStr = "hello World"
@@ -18,7 +32,7 @@ const wordArr = noWhiteSpaceStr.split("")
 // Function returning array of string with Uppercase first character
 const returnAdjustedChar = (strArr) => {
   return strArr.map((char, index) => {
-    console.log(index)
+    // console.log(index)
     return char.toUpperCase() + char.toLowerCase().repeat(index)
   })
 } 
@@ -28,4 +42,4 @@ const returnAdjustedChar = (strArr) => {
 // Convert an Array of String to a String
 const convertTestArr = ["a", "b", "c"]
 const resultTestArr = convertTestArr.join("-")
-console.log(resultTestArr)
+// console.log(resultTestArr)
