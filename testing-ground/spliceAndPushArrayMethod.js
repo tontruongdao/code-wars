78be3fb83334beef4c91e41b9f40ccbc385a7a86
truongsinistar <truong@sinistar.ca>
2021-09-15 12:04:23 -0400

    used filter method

--- testing-ground/spliceAndPushArrayMethod.js
+++ testing-ground/spliceAndPushArrayMethod.js
@@ -1,19 +1,16 @@
 const fruitsArr = ["banana", "orange", "pear"]
 const tempArr = []
 
-if (fruitsArr.includes("orange")) {
-  console.log("array includes orange")
-  const itemIdx = fruitsArr.findIndex((fruit) => fruit === "orange")
-  console.log(itemIdx)
-  tempArr.push("orange")
-  fruitsArr.splice(itemIdx, 1)
-}
+// if (fruitsArr.includes("orange")) {
+//   console.log("array includes orange")
+//   const itemIdx = fruitsArr.findIndex((fruit) => fruit === "orange")
+//   console.log(itemIdx)
+//   tempArr.push("orange")
+//   fruitsArr.splice(itemIdx, 1)
+// }
 
-console.log("fruitsArr:", fruitsArr)
-console.log("tempArr is:", tempArr)
+// console.log("fruitsArr:", fruitsArr)
+// console.log("tempArr is:", tempArr)
 
-
-const requestID = "weO6L8CoFNbuwPWV7kmH"
-
-const housingID = "AgVgCgFH1KZlcxUue3Tf"
-const notGoodId = "yavxVlHaiNvK8O5UNiHX"
+const filteredArr = fruitsArr.filter((fruit) => fruit === "orange")
+console.log(filteredArr)
