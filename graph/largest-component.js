ca58f1776c37bc8c3a2166987aa388bcaa1039bf
truongsinistar <truong@sinistar.ca>
2022-02-13 14:53:39 -0500

    graph largest-component

---
+++ graph/largest-component.js
@@ -0,0 +1,37 @@
+const graph = {
+  0: ['8', '1', '5'],
+  1: ['0'],
+  5: ['0', '8'],
+  8: ['0', '5'],
+  2: ['3', '4'],
+  3: ['2', '4'],
+  4: ['3', '2']
+}; // -> 4
+
+const largestComponent = (graph) => {
+  const visited = new Set()
+  let largest = 0;
+
+  for (let node in graph) {
+    const size = exploreSize(graph, node, visited)
+
+    if (size > largest) largest = size;
+  }
+
+  return largest;
+}
+
+const exploreSize = (graph, node, visited) => {
+  if (visited.has(node)) return 0;
+  visited.add(node);
+
+  let size = 1;
+
+  for (let neighbor of graph[node]) {
+    size += exploreSize(graph, neighbor, visited);
+  }
+
+  return size;
+}
+
+console.log(largestComponent(graph))
