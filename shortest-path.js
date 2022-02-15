202cf5bc9b684f624e31d0af0d45b63d2cf9134b
truongsinistar <truong@sinistar.ca>
2022-02-14 20:11:05 -0500

    solution shortest path

---
+++ shortest-path.js
@@ -0,0 +1,48 @@
+const edges = [
+  ['w', 'x'],
+  ['x', 'y'],
+  ['z', 'y'],
+  ['z', 'v'],
+  ['w', 'v']
+]; // -> 2
+
+const shortestPath = (edges, firstNode, secondNode) => {
+  const graph = buildGraph(edges);
+  console.log("graph", graph)
+  const visited = new Set([firstNode])
+
+  const queue = [[firstNode, 0]];
+
+  while (queue.length > 0) {
+    const [node, distance] = queue.shift();
+
+    if (node === secondNode) return distance;
+
+    for (let neighbor of graph[node]) {
+      if (!visited.has(neighbor)) {
+        visited.add(neighbor);
+        queue.push([neighbor, distance + 1])
+        console.log("queue is ---> ", queue)
+      }
+    }
+  }
+
+  return -1; // If nothing returns -1
+}
+
+const buildGraph = (edges) => {
+  const graph = {};
+
+  for (let edge of edges) {
+    const [a, b] = edge;
+    if (!(a in graph)) graph[a] = [];
+    if (!(b in graph)) graph[b] = [];
+    graph[a].push(b);
+    graph[b].push(a);
+
+  }
+
+  return graph;
+}
+
+console.log(shortestPath(edges, 'w', 'z')); 
